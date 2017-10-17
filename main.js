fe4508a745415035db15b7049fe742f942991ccc
valeria <valeriairaismeneses@gmail.com>
2017-10-17 12:43:20 -0500

    Contador

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ var div = document.querySelector(".contador")
 var intervalo = setInterval(function() {
   div.innerHTML = counter
   counter--
-
+  if (div.innerHTML === '0') {
+    div.textContent = "Boom!"
+    counter= 10
+  }
   if (counter < 0) {
     clearInterval(intervalo)
   }
-  if (div.innerHTML === '0') {
-    div.textContent = "Boom!"
-  }
+
 }, 1000)
